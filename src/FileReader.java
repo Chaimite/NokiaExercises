b4b1e5f5eb73d88ccb2bda14b995bf6b356c33f0
Chaimite <adrianocorreiacesar@gmail.com>
2019-05-29 12:39:03 +0100

    Fixed file reader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,11 +1,15 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileReader
 {
-    // Get field names contained in .csv file
+ 
+
+   // Get field names contained in .csv file
    public static String[] getFieldNames(String filePath)
    {
       File file = new File(filePath);
@@ -28,19 +32,17 @@ public class FileReader
    public static ArrayList<String> getAlarmNumber(String filePath)
    {
       File file = new File(filePath);
+      FileReader fr = null;
       ArrayList<String> parsedValues = new ArrayList<String>();
       try
       {
          Scanner inputStream = new Scanner(file);
-         inputStream.next();
-//         inputStream.useDelimiter(";");
-         while (inputStream.hasNext())
-         {
-            
-            String data = inputStream.next();
-//            System.out.println(data + "***");
-//            String[] values = data.split(";");
-            parsedValues.add(data);
+         inputStream.next();      
+         while (inputStream.hasNextLine())
+         {            
+            String data = inputStream.nextLine();
+            String[] values = data.split(";");
+            parsedValues.add(values[0].toString());
          }
          inputStream.close();
       }
@@ -50,10 +52,24 @@ public class FileReader
       }
       return parsedValues;
    }
+   
+   
+   // Check if state is cancelled
+   public boolean isStateNotCanceled(String state)
+   {
+      boolean value = true;
+      if (!state.equals("cancelled"))
+      {
+         value = true;
+      }
+      return value;
+   }
+   
    public static void main(String[]args) {
 //      String filePath = System.getProperty("user.dir") + "\\alarms.csv";
       String filePath = "C:\\Users\\Adriano\\eclipse-workspace\\Nokia_Exercise\\src\\alarms.csv";
 //      System.out.println(getFieldNames(filePath)[0]);
-      System.out.println(getAlarmNumber(filePath).get(0));
+      System.out.println(getAlarmNumber(filePath));
+
    }
 }
