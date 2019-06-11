711ff0a650e03c6267872bfec4fc3f04c13cbc68
Chaimite <adrianocorreiacesar@gmail.com>
2019-06-11 16:17:57 +0100

    added interface for file path

--- src/testing/testAlarm.java
+++ src/testing/testAlarm.java
@@ -2,11 +2,16 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+
+import model.Alarm;
 
 class testAlarm
 {
-
+   @BeforeClass
+   Alarm newAlarm = new Alarm("25", "processingError", "network failure", "new", "01-01-2019  10:20:00");
+   
    @Test
    void test()
    {
